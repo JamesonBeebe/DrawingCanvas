0e8e3b4160fbd32978564eff8025c83c5762146c
Jameson Beebe <jamesonbeebe@gmail.com>
2015-04-30 15:40:43 -0500

    initial commit

---
+++ Introduction.java
@@ -0,0 +1,12 @@
+mport java.io.Console;
+ 
+public class Introductions {
+  
+    public static void main(String[] args) {
+        Console console = System.console();
+        // Welcome to the Introductions program!  Your code goes below here
+        String firstName = "Stephany";
+        console.printf("Hello World \n");
+        console.printf("%s is learning how to write Java\n", firstName);
+  }
+}
